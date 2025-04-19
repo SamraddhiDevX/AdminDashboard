da6207452bef30237501516d35194e88a8d08d56
Samraddhi <158586105+SamraddhiDevX@users.noreply.github.com>
2025-04-19 23:57:06 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,8 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
+  sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000,
   });
 };
