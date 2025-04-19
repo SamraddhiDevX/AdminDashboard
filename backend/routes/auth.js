5ad22c03ab4ea3b91aebfc6576134c1847f40d80
Samraddhi <158586105+SamraddhiDevX@users.noreply.github.com>
2025-04-20 01:07:28 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,12 +10,14 @@ const router = express.Router();
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
+ console.log("token:":token)
+res.cookie('jwt', token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'Lax',
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
-  res.cookie('jwt', token, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000,
-  });
-};
 
 // @route POST /api/auth/login
 router.post('/login', async (req, res) => {
