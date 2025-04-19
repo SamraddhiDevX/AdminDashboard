2b3aa71735f25a6b5507c0a972ec817a098625d9
Samraddhi <158586105+SamraddhiDevX@users.noreply.github.com>
2025-04-20 01:23:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosIns= axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5000/api":"https://admindashboardback-mrkr.onrender.com/api",
+     baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
     withCredentials:true,
 })
